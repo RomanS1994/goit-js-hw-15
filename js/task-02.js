fe656172634ca516ce273751defffb63d66de35a
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-13 21:50:15 +0100

    update task#2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+// console.log(ingredients);
+const listIngredients = document.querySelector("#ingredients");
+// console.log(listIngredients);
+
+const createMarkup = ingredients.map((ingredient) => {
+  let li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ingredient;
+  return li;
+});
+
+console.log(createMarkup);
+listIngredients.append(...createMarkup);
