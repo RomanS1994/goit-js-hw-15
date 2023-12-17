5304215d3d9247fcd95c2860d726624e2eef26b3
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-17 17:53:32 +0100

    update task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const registerForm = document.querySelector(".login-form");
+// console.dir(registerForm);
 
+registerForm.addEventListener("submit", handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+  const email = evt.currentTarget.elements.email.value;
+  const password = evt.currentTarget.elements.password.value;
+  //   console.log(email);
+  //   console.log(password);
+
+  if (email === "" || password === "") {
+    alert("Please fill in all the fields!");
+    return;
+  }
+  const result = {
+    email: email,
+    password: password,
+  };
+  console.log(result);
+  registerForm.reset();
+}
